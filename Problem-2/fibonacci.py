4be2bf4bcfbfa51179af700c3ff9c07b40acf453
Anton <antonilic7@gmail.com>
2023-02-25 18:59:34 -0800

    Added solution for Fibonacci. Note that this is not efficient timewise

---
+++ Problem-2/fibonacci.py
@@ -0,0 +1,15 @@
+#Sum of even valued Fibonacci terms, up to 4 million
+#Note: This method is the slow way. Can also calculate via properties of fibonaci numbers, notably that every 3rd number is even (ie. Two odd numbers make an even number)
+totalSumOfModTwos = 0
+previousFib = 1; nextFib = 1
+i = 0
+
+while(i < 4000000):
+    if(i%2 == 0):
+        totalSumOfModTwos += i
+    nextFib = i
+    i = previousFib + nextFib
+    previousFib = nextFib
+
+    
+print(totalSumOfModTwos)

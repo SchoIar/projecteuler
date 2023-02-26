a9ea68b324a283b46a0bd1a49e8f7fcfaf644119
Anton <antonilic7@gmail.com>
2023-02-25 19:40:26 -0800

    update to comments

--- Problem-2/fibonacci.py
+++ Problem-2/fibonacci.py
@@ -1,3 +1,6 @@
+#Anton Ilic, Feb 25, 2023
+#https://projecteuler.net/problem=2
+
 #Sum of even valued Fibonacci terms, up to 4 million
 #Note: This method is the slow way. Can also calculate via properties of fibonaci numbers, notably that every 3rd number is even (ie. Two odd numbers make an even number)
 totalSumOfModTwos = 0
---
+++ Problem-3/largestPrimeFactor.py
@@ -0,0 +1,3 @@
+#Anton Ilic, Feb 25, 2023
+#https://projecteuler.net/problem=3
+#Find the largest prime factor of 600851475143 

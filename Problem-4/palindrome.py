f64b4ecb03d09a139309d047b53cf94d74af257f
Anton <antonilic7@gmail.com>
2023-02-27 19:33:53 -0800

    solution for Palindrome finder

---
+++ Problem-4/palindrome.py
@@ -0,0 +1,26 @@
+#Anton Ilic, Feb 27, 2023
+#https://projecteuler.net/problem=4
+
+#Find the largest palindrome made from the product of two 3-digit numbers.
+#ie. 91x99 = 9009
+
+def Reverse(myString):
+    #reverse string
+    myStringReversed = ''
+    for character in myString:
+        myStringReversed = character + myStringReversed
+    return myStringReversed
+
+def isPalindrome(number):
+    if(str(number) == Reverse(str(number))):
+        return True
+    return False    
+
+#Try all combinations to see which numbers are palindromes, store them in a list, choose the largest.
+a = []
+
+for i in range(1, 999):
+    for j in range(1, 999):
+        product = i * j
+        if(isPalindrome(product)):
+            a.append(product)

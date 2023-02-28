020d4aae47c7ca3c1a73950d93857baea0ba234d
Anton <antonilic7@gmail.com>
2023-02-27 21:21:13 -0800

    Sum of squares problem

--- Problem-6/sumOfSquares.py
+++ Problem-6/sumOfSquares.py
@@ -1,3 +1,5 @@
+#Anton Ilic, Feb 27, 2023
+#https://projecteuler.net/problem=6
 #Find the difference between the sum of the squares of the 
 #first one hundred natural numbers and the square of the sum.
 sumOfSquares = 0
@@ -7,5 +9,4 @@ for i in range(0,11):
     squareOfSum += i
 
 squareOfSum = squareOfSum * squareOfSum
-
 print(abs(sumOfSquares - squareOfSum))

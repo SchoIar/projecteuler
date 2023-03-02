2fd37770094ed6312ee5e561866c079224bc5223
Anton <antonilic7@gmail.com>
2023-03-01 21:44:48 -0800

    comments

--- Problem-10/sumOfPrimes.py
+++ Problem-10/sumOfPrimes.py
@@ -1,19 +1,23 @@
-#Anton Ilic, Mar 1, 2023
-#https://projecteuler.net/problem=10
-#Sum of all primes under two million
+# Anton Ilic, Mar 1, 2023
+# https://projecteuler.net/problem=10
+# Sum of all primes under two million
 def findPrime():
     primes = [2]
     i = 3
-    primeSum = 2
-    while(i < 2000000):
+    
+    while (i < 2000000):
         isPrime = True
         for primeNumber in primes:
-            if(i%primeNumber == 0):
-                #not a prime number
+            if (i % primeNumber == 0):
+                # not a prime number
                 isPrime = False
-        if(isPrime):
-            #primes.append(i)
-            primeSum += i
+        if (isPrime):
+            primes.append(i)
+
         i = i + 2
-    return primeSum
+    return sum(primes)
     #primeSum = sum(primes)
+
+#Note: This will give the right answer? but is super slow (20+m) May try to optimize later. 
+
+print(findPrime())

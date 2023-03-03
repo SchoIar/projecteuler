7041d57fb421c310a513e1a6d9a8d088a6dc7932
Anton <antonilic7@gmail.com>
2023-03-02 20:48:45 -0800

    final revision

--- Problem-9/pythag.py
+++ Problem-9/pythag.py
@@ -9,3 +9,4 @@ for a in range(1,1001):
     for b in range(1, 1001):
         if((a + b + ((a**2+b**2)**0.5)) == 1000):
             print(f"Product of a, b and c " + str(a*b*int((a**2+b**2)**0.5)))
+            break

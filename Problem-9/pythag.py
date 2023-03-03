f0d2ef57154497787c515e9b21048018448e8209
Anton <antonilic7@gmail.com>
2023-03-02 20:41:26 -0800

    Removed old code

--- Problem-9/pythag.py
+++ Problem-9/pythag.py
@@ -4,13 +4,6 @@
 
 #Very slow solution for Problem 9. Could be done with two loops, by calculating the 3rd variable. (using a+b+c = 1000)
 #Will try to optimize later. 
-"""
-for a in range(1,1001):
-    for b in range(1, 1001):
-        for c in range(1,1001):
-            if(a**2 + b**2 == c**2 and a + b + c == 1000):
-                print(f"A: {a} B: {b} C: {c}")      
-"""
                 
 for a in range(1,1001):
     for b in range(1, 1001):

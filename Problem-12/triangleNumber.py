290ba8937313ff991c97ab8f42b508d8f5bcd716
Anton <antonilic7@gmail.com>
2023-03-04 21:00:35 -0800

    experimental code

--- Problem-12/triangleNumber.py
+++ Problem-12/triangleNumber.py
@@ -16,3 +16,25 @@ while(calculateDivisors(i) < 500):
     i += n; n += 1
     
 print(i)
+
+'''
+def calculateDivisors(number):
+    #divisiorsFound = []
+
+    numOfDivisors = [] #removes redudant calculation for the number itself and 1. 
+    for i in range(1,number+1):
+        if(number%i == 0):
+            numOfDivisors.append(i)
+    #return len(divisiorsFound)
+    return numOfDivisors
+
+listOfDiv = []
+i = 0; n = 1 #i for iterated numbr, n for difference in triangle num.
+while(len(calculateDivisors(i)) < 500):
+    i += n; n += 1
+    print(len(calculateDivisors(i)))#,end=" ")
+    listOfDiv.append(len(calculateDivisors(i)))
+    #print(calculateDivisors(i))
+    
+print(listOfDiv)
+'''

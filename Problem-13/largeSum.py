6db77968671315282555ba714014e74e5af67833
Anton <antonilic7@gmail.com>
2023-03-05 16:43:05 -0800

    Solution to Problem-13

--- Problem-13/largeSum.py
+++ Problem-13/largeSum.py
@@ -1,3 +1,12 @@
 # Anton Ilic, Mar 5, 2023
 #https://projecteuler.net/problem=13
 
+#Note: I solved this problem using XLS, then came back to it in python.
+numbers = []
+with open('numbers.txt') as f:
+    for line in f:
+        numbers.append(int(line.strip()))
+
+solution = sum(numbers)
+solution = str(solution)[0:10] 
+print(solution)

666202a6d7d0a0ac6e3c02889eead69c14d95a4c
Anton <antonilic7@gmail.com>
2023-03-05 17:47:53 -0800

    solution to problem 14

--- Problem-14/largestChain.py
+++ Problem-14/largestChain.py
@@ -3,3 +3,25 @@
 
 #n → n/2 (n is even)
 #n → 3n + 1 (n is odd)
+longestChainStart = 1; maxChainLength = 1
+
+def collatz(num):
+    numCount = 1
+    while(num != 1):
+        if(num%2 == 0): #Case: Even
+            num = int(num / 2)    
+        else:
+            num = int(3*num + 1)
+        numCount += 1
+    return numCount
+
+for i in range(1,1000001): #Up to 1 million. 
+#to lower being mult.  
+    chainLength = collatz(i)
+    if (maxChainLength < chainLength):
+        longestChainStart = i
+        maxChainLength = chainLength
+        print(f"Start: {longestChainStart} Length:{maxChainLength}") 
+    i += 1
+
+        

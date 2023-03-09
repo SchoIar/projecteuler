d928e0114c6c1475e273f2c7b91d54cd1ad96061
Anton <antonilic7@gmail.com>
2023-03-08 20:55:27 -0800

    Inserting numbers into array

--- Problem-18/minPathSum.py
+++ Problem-18/minPathSum.py
@@ -1,2 +1,11 @@
 # Anton Ilic, Mar 6, 2023
 #https://projecteuler.net/problem=18
+
+numbers = []
+with open('data.txt') as f:
+    for line in f:
+        numToInsert = []
+        for field in line.split():
+            numToInsert.append(int(field))
+        numbers.append(numToInsert)
+print(numbers)

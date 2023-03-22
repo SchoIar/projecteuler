2767e9b60578a59728aee61d28c545710422c571
Anton <antonilic7@gmail.com>
2023-03-21 20:36:44 -0700

    potential BST implimentation

--- Problem-18/minPathSum.py
+++ Problem-18/minPathSum.py
@@ -1,6 +1,22 @@
 # Anton Ilic, Mar 6, 2023
 #https://projecteuler.net/problem=18
 
+#Potential to use a BST
+"""class Node:
+    def __init__(self, data, parent, left, right):
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+
+class Tree:
+    def __init__(self, head):
+        self.head = None
+    
+    def 
+"""
+
+
 numbers = []
 with open('data.txt') as f:
     for line in f:
@@ -8,4 +24,3 @@ with open('data.txt') as f:
         for field in line.split():
             numToInsert.append(int(field))
         numbers.append(numToInsert)
-print(numbers)

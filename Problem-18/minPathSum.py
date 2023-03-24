18fff514de97e0253f653b53fd214aa2b1034ed4
Anton <antonilic7@gmail.com>
2023-03-23 22:06:52 -0700

    adding to 0th

--- Problem-18/minPathSum.py
+++ Problem-18/minPathSum.py
@@ -3,12 +3,17 @@
 
 def findMaxTotal(numberList):
     numberOfRows = len(numberList)
-
+    maxSum = 0; sum = numberList[0][0]
     #00
     #10 11
     #20 21 22 
     #30 31 32 33
-    
+    print(numberList[numberOfRows-1][len(numberList[numberOfRows-1])-1])#print the last number
+
+    for i in range(0,15):
+        #adds to zeroth path
+        sum += numberList[i][0]
+
 
 
     return numberOfRows
@@ -21,3 +26,4 @@ with open('data.txt') as f:
             numToInsert.append(int(field))
         numbers.append(numToInsert)
 
+findMaxTotal(numbers)

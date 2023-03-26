66dfc7233e59888e8834acb162aa8dc6a751a6a3
Anton <antonilic7@gmail.com>
2023-03-26 16:47:54 -0700

    fixed findnewMaxLayer

--- Problem-18/minPathSum.py
+++ Problem-18/minPathSum.py
@@ -3,15 +3,30 @@
 
 def findnewMaxLayer(numberList):
     ''''Returns new 'bottom' layer by finding maximum values from other layers'''
-    #return numberList.pop(len(numberList)-1)
+    #TODO: Add code to 'condense' bottom two rows, and replace index len-2 w it.
+
+    newList = []
+    number = 0
+    for i in numberList[len(numberList)-2]:
+        numberToAdd = i + numberList[len(numberList)-1][number+1]#max(numberList[len(numberList)-1][number],numberList[len(numberList-1)][number+1])
+        secondNumberToAdd = i + numberList[len(numberList)-1][number]
+        number += 1
+        newList.append(max(numberToAdd, secondNumberToAdd))
+
+    #numberList[len(numberList)-1] = newList
+
+    numberList.pop(len(numberList)-2)
+    numberList.pop(len(numberList)-1)
+    numberList.append(newList)
+    return numberList
 
 def findMaxSum(numberList):
     '''Finds optimal path's sum by finding maximum sum by 'layer'.'''
-    numberOfRows = len(numberList)
-    if(numberOfRows == 1):
-        print(numberList)
-        return numberList[0][0]
+
+    if(len(numberList) == 1):
+        return numberList
     else:
+        print(numberList)
         findnewMaxLayer(numberList)
 
     findMaxSum(numberList)

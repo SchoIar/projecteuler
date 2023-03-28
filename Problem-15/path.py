6b2777b63e343e6d00574935dba6ba7744dd3db5
Anton <antonilic7@gmail.com>
2023-03-27 20:48:38 -0700

    representation of 20x20

--- Problem-15/path.py
+++ Problem-15/path.py
@@ -1,2 +1,19 @@
 # Anton Ilic, Mar 6, 2023
 #https://projecteuler.net/problem=15
+
+#Representing 20 x 20 grid by it's coordinates, then performing a breadth first search
+
+grid = []
+line = []
+for x in range(0, 21):
+    line = []
+    for y in range(0, 21):
+        a = (x, y)
+        line.append(a)
+    grid.append(line)
+        
+print(grid)
+
+
+#See https://en.wikipedia.org/wiki/Permutation for mathematical explanation for problem.
+

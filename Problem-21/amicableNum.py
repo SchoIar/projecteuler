828d592b73210f74e2ca006895735e3f6b7f8af4
Anton <antonilic7@gmail.com>
2023-04-01 21:58:48 -0700

    final commit

--- Problem-21/amicableNum.py
+++ Problem-21/amicableNum.py
@@ -1,3 +1,20 @@
 # Anton Ilic, completed Apr 1, 2023
 # https://projecteuler.net/problem=21
 
+def findDivisorsSum(number):
+    divisorSum = 0
+    for i in range(1, number):
+        if(number%i == 0):
+            divisorSum += i
+    return divisorSum
+
+def findAmicableNumbersSum():
+    sumOfAmicableNumbers = 0
+    for i in range(1, 10000):
+        divisorSum = findDivisorsSum(i)
+        if findDivisorsSum(divisorSum) == i and divisorSum != i:
+            sumOfAmicableNumbers += i
+    return sumOfAmicableNumbers
+
+print(findAmicableNumbersSum())
+

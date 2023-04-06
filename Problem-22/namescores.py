be375ce6e46cd638101b3334fbaad337a3dc2303
Anton <antonilic7@gmail.com>
2023-04-05 17:23:57 -0700

    linkedlist implimentation

--- Problem-22/namescores.py
+++ Problem-22/namescores.py
@@ -1,4 +1,27 @@
+class LinkedList():
+    def __init__(self):
+        self.head = None
+    
+    def insert(self, name):
+        if(self.head == None):
+            self.head = Node(name)
+        else:
+            tempNode = self.head
+            while(tempNode.next != None):
+                tempNode = tempNode.next
+            tempNode.next = Node(name)
+
+class Node():
+    def __init__(self, name):
+        self.name = name
+        self.calculatedValue = None
+        self.next = None
+
+
 with open("names.txt", "r+") as file:
     for line in file:
         currentLine = line.strip().replace('"','').split(',')
-        print(currentLine)
+        
+
+for element in currentLine:
+    pass #insert in sorted order here

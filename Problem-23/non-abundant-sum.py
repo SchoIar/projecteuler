12ca79fd8356a6aef8413c4930b4ad0268d69dc5
Anton <antonilic7@gmail.com>
2023-04-07 15:39:12 -0700

    first commit

---
+++ Problem-23/non-abundant-sum.py
@@ -0,0 +1,6 @@
+# Anton Ilic, completed Apr 7, 2023
+# https://projecteuler.net/problem=23
+
+# "By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers"
+for i in range(28123, 0, -1):
+    pass

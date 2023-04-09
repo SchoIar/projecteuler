3b16b2c8dd6355ad694ea5940a6f35fc31931ad6
Anton <antonilic7@gmail.com>
2023-04-08 18:25:09 -0700

    completed solution:

--- Problem-23/non-abundant-sum.py
+++ Problem-23/non-abundant-sum.py
@@ -1,5 +1,12 @@
 # Anton Ilic, completed Apr 7, 2023
 # https://projecteuler.net/problem=23
+MAXVALUE = 28123
+def CalculateAbundantList():
+    listOfNumbers = []
+    for i in range(12,MAXVALUE+1):#(1, 28124):
+        if(isAbundant(i)):
+            listOfNumbers.append(i)
+    return listOfNumbers
 
 def findFactors(number):
     factors = []
@@ -14,8 +21,21 @@ def isAbundant(number):
     else: #'perfect' number or non-abundant
         return False 
 
-listOfNumbers = []
-for i in range(12,28124):#(1, 28124):
-    if(isAbundant(i)):
-        listOfNumbers.append(i)
+def findSumOfValues():
+    listOfSums = []
+    listOfAbundants = CalculateAbundantList()
+    print('CALLED FINDSUMOFVALUES')
+    for firstNumber in listOfAbundants:
+        for secondNumber in listOfAbundants: 
+            sumOfAbundants = firstNumber + secondNumber
+            if sumOfAbundants > MAXVALUE:
+                break
+            else:
+                listOfSums.append(sumOfAbundants)
+    sumOfValues = 0
+    print('COMPLILING LIST')
+    listOfNumbers = [*range(0, MAXVALUE+1)]
+    l = list(set(listOfNumbers) - set(listOfSums))
+    return sum(l)
 
+print(findSumOfValues())

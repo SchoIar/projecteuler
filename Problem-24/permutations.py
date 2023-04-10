673ed2072c7ae67934717e1bc2f726eff264d2a0
Anton <antonilic7@gmail.com>
2023-04-09 21:11:03 -0700

    condensed code

--- Problem-24/permutations.py
+++ Problem-24/permutations.py
@@ -1,5 +1,25 @@
 # Anton Ilic, Apr 8, 2023
 # https://projecteuler.net/problem=24
 
+def convertToInt(selectedListOfInt):
+    number = ''
+    for element in selectedListOfInt:
+        number += str(element)
+    return int(number)
+
+
+listOfCombos = []
 listOfOneToTen = [*range(0, 10)]
 
+for a in range(0, 10):
+    for i in listOfOneToTen:
+        for j in listOfOneToTen:
+            tempElement = listOfOneToTen[i]
+            listOfOneToTen[i] = listOfOneToTen[j]
+            listOfOneToTen[j] = tempElement
+
+            listOfCombos.append(convertToInt(listOfOneToTen))
+
+
+
+print(len(listOfCombos))

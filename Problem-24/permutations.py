92d690d9d6ca1123617b19855ec41899d49b25b5
Anton <antonilic7@gmail.com>
2023-04-11 22:26:13 -0700

    added pseudocode for algorithm

--- Problem-24/permutations.py
+++ Problem-24/permutations.py
@@ -1,5 +1,6 @@
 # Anton Ilic, Apr 8, 2023
 # https://projecteuler.net/problem=24
+# Note, I found a solution, but it was extremly slow. To speed it up, I read about the 'generation in lexical order' algorithm found here: https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
 
 def convertToInt(selectedListOfInt):
     number = ''
@@ -7,16 +8,27 @@ def convertToInt(selectedListOfInt):
         number += str(element)
     return int(number)
 
-def swap(array, indexOne, indexTwo):
-    '''Swaps array indicies One and Two'''
-    temp = array[indexOne]
-    array[indexOne] = array[indexTwo]
-    array[indexTwo] = temp
-    return array
 
-def getSortedArray():
-    #sort in order here, return millionth element 
-    pass
+def getNextPermutation(currentPermutation):
+    
+    # find a[k] < a[k+1] ie. 87132 where k = [2] 
+    # find largest index l where l > k, such that a[k] < a[l] ie. l is [4] 
+    # swap a[k] and a[l] ie. 87132 becomes 87231
+    # reverse from a[k+1] up to final element is reversed (ie. 872-[31] -> 87213
+    
+
+    return currentPermutation
+
+listOfCombos = []
+listOfOneToTen = [*range(0, 10)]
+listOfOneToTen = convertToInt(listOfOneToTen)
+
+for i in range(1, 1000001):
+    #listOfCombos.append(listOfOneToTen)
+    listOfOneToTen = getNextPermutation(listOfOneToTen)
+
+print(listOfOneToTen)
+
     
 
 

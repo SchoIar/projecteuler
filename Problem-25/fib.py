6ab0f7467329c4a2ccefe4f64a5daa54690dfa70
Anton <antonilic7@gmail.com>
2023-04-14 21:07:11 -0700

    formatted code

--- Problem-25/fib.py
+++ Problem-25/fib.py
@@ -5,17 +5,17 @@
 def FindNextFib(FibList):
     '''Finds the next fibonacci number in the list'''
     previousFibIndex = len(FibList) - 1
-    FibList.append(int(FibList[previousFibIndex]) + int(FibList[previousFibIndex-1]))
+    FibList.append(int(FibList[previousFibIndex]) +
+                   int(FibList[previousFibIndex-1]))
     return FibList
+
 
 def FindFibonacciOfNLength(n):
     '''Returns index of first Fibonacci number of N length'''
-    Elements = [1,1]
-
-    while(len(str(Elements[(len(Elements)) - 1])) < n):
+    Elements = [1, 1]
+    while (len(str(Elements[(len(Elements)) - 1])) < n):
         Elements = FindNextFib(Elements)
-        
-        
     return len(Elements)
+
 
 print(FindFibonacciOfNLength(1000))

4425689e0c88a2ce0cb41c9c8a4946f0347fa80d
Anton <antonilic7@gmail.com>
2023-04-16 11:42:07 -0700

    updated FindNumOfRepeatingDigits

--- Problem-26/cycles.py
+++ Problem-26/cycles.py
@@ -1,16 +1,22 @@
 # Anton Ilic, Apr 15, 2023
 # https://projecteuler.net/problem=26
-
-#def findNumOfRepeatingDigitsHelper(reciprocal):
+#
 
 def findNumOfRepeatingDigits(selectedNumber):
     '''Returns the number of repeating digits '''
     reciprocal = 1/selectedNumber
-
-    return selectedNumber
+    reciprocal = str(reciprocal) 
+    reciprocal = reciprocal[2::] #removing '0.' characters
+    for i in range(0, len(reciprocal)):
+        reciprocalSlice = reciprocal[i::]
+        if reciprocalSlice * (len(reciprocal) // len(reciprocalSlice)) in reciprocal:
+            #Checking if the repeating slice (substring) can be repeated n times and in contents of the original string, the reason for ignoring remainder is that, the non repeated characters have to be ignored
+            return len(reciprocalSlice)
+        
+    return 0
 
 def findLargestRecurringCycle(maxNum):
-
+    '''Finds largest recurring cycle for reciprocal of 1/2 to 1/n'''
     potentialLargestNum = 0; potentialLargestNumDigits = 0
     for currentNumber in range(2, maxNum + 1):
         #if the number has more digitsn then the previous potentialLargest digits, set it to potentialLargest
@@ -21,4 +27,5 @@ def findLargestRecurringCycle(maxNum):
 
     return potentialLargestNum
 
-print(findLargestRecurringCycle(10))
+if __name__ == '__main__':
+    print(findLargestRecurringCycle(1000))

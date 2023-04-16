549109ff05350e3cd73892bbc2c6a1aa1c0d00f2
Anton <antonilic7@gmail.com>
2023-04-15 20:41:19 -0700

    base code

--- Problem-26/cycles.py
+++ Problem-26/cycles.py
@@ -1,2 +1,17 @@
 # Anton Ilic, Apr 15, 2023
 # https://projecteuler.net/problem=26
+
+#def findNumOfRepeatingDigitsHelper(reciprocal):
+
+def findNumOfRepeatingDigits(selectedNumber):
+    reciprocal = 1/selectedNumber
+
+    return selectedNumber
+
+def findLargestRecurringCycle(maxNum):
+    potentialLargest = 0
+    for largestRepeatingNum in range(2, maxNum + 1):
+        potentialLargest = max(findNumOfRepeatingDigits(largestRepeatingNum), potentialLargest)
+    return potentialLargest
+
+print(findLargestRecurringCycle(10))

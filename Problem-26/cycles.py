afd6e3be6904871c04919a1e758deee1e9596b9f
Anton <antonilic7@gmail.com>
2023-04-16 21:06:18 -0700

    working solution

--- Problem-26/cycles.py
+++ Problem-26/cycles.py
@@ -1,19 +1,15 @@
-# Anton Ilic, Apr 15, 2023
+# Anton Ilic, Apr 16, 2023
 # https://projecteuler.net/problem=26
-#
 
 def findNumOfRepeatingDigits(selectedNumber):
     '''Returns the number of repeating digits '''
-    reciprocal = 1/selectedNumber
-    reciprocal = str(reciprocal) 
-    reciprocal = reciprocal[2::] #removing '0.' characters
-    for i in range(0, len(reciprocal)):
-        reciprocalSlice = reciprocal[i::]
-        if reciprocalSlice * (len(reciprocal) // len(reciprocalSlice)) in reciprocal:
-            #Checking if the repeating slice (substring) can be repeated n times and in contents of the original string, the reason for ignoring remainder is that, the non repeated characters have to be ignored
-            return len(reciprocalSlice)
+    remainder = 1; remainderList = [] #starting at 1, since we take the remainder of the numerator (ie. 1/n, remainder is 1)
+    while remainder not in remainderList: #essentially performing long division by hand till reach 0.
+        remainderList.append(remainder)
+        remainder = (remainder * 10) % selectedNumber #shifting decimal right
+    return len(remainderList)
+
         
-    return 0
 
 def findLargestRecurringCycle(maxNum):
     '''Finds largest recurring cycle for reciprocal of 1/2 to 1/n'''
@@ -24,7 +20,6 @@ def findLargestRecurringCycle(maxNum):
         if potentialLargestNumDigits < currentDigits:
             potentialLargestNum = currentNumber
             potentialLargestNumDigits = currentDigits
-
     return potentialLargestNum
 
 if __name__ == '__main__':

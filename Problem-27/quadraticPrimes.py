9725f989fbaddc3628587425aa3f3e661e3321c6
Anton <antonilic7@gmail.com>
2023-04-17 17:55:01 -0700

    handled negative, 0, 1 input for isPrime

--- Problem-27/quadraticPrimes.py
+++ Problem-27/quadraticPrimes.py
@@ -2,7 +2,10 @@
 # https://projecteuler.net/problem=27
 
 def isPrime(number):
-    for i in range(2, number):
+    if number < 2: #-ive, 0, 1 return false.
+        return False
+    
+    for i in range(2, number // 2 + 1):
         if(number%i == 0):
             return False
     return True
@@ -11,21 +14,21 @@ def findNumberOfPrimes(a, b):
     i = 0
     while isPrime(i**2 + a * i + b): #note that, by definition, since we are starting at 0, b must be prime
         i += 1
-    return i + 1 # note that '0' is also a prime. 
+    return i
 
 def solution():
     maxPrimesUpTo = 0; alpha = 0; beta = 0
 
     for a in range(-999, 1000):
-        print(a)
         for b in range(0, 1000): 
             currentPrimesUpTo = findNumberOfPrimes(a, b)
             if currentPrimesUpTo > maxPrimesUpTo:
                 maxPrimesUpTo = currentPrimesUpTo
                 alpha = a
                 beta = b
+                
 
-    return (alpha, beta)     
+    return alpha * beta
 
 if __name__ == '__main__':
     print(solution())

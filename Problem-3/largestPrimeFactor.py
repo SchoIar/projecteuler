598a7ecf59094d64a654b52811cfcae03e8450c7
Anton <antonilic7@gmail.com>
2023-04-17 18:23:25 -0700

    final commit

--- Problem-3/largestPrimeFactor.py
+++ Problem-3/largestPrimeFactor.py
@@ -1,7 +1,6 @@
 #Anton Ilic, Feb 25, 2023, revised April 17, 2023
 #https://projecteuler.net/problem=3
 
-#Taking from the bottom
 def largestPrimeFactorFinder(number):
     i = 2
     while i ** 2< number:
@@ -10,7 +9,6 @@ def largestPrimeFactorFinder(number):
         i = i + 1
 
     return int(number)
-
 
 number = 600851475143 
 print(largestPrimeFactorFinder(number))

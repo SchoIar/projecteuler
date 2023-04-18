875c76a5f291e26235cc842bed101f9f64394028
Anton <antonilic7@gmail.com>
2023-04-17 18:23:08 -0700

    optimized solution, final commit

--- Problem-3/largestPrimeFactor.py
+++ Problem-3/largestPrimeFactor.py
@@ -1,25 +1,16 @@
-#Anton Ilic, Feb 25, 2023
+#Anton Ilic, Feb 25, 2023, revised April 17, 2023
 #https://projecteuler.net/problem=3
-#Find the largest prime factor of 600851475143
-#very inneficiant and slow, but will eventually give the right answer 
-
-def isPrime(number):
-    if number < 2: #-ive, 0, 1 return false.
-        return False 
-    for i in range(2, number // 2 + 1):
-        if(number%i == 0):
-            return False
-    return True
 
 #Taking from the bottom
 def largestPrimeFactorFinder(number):
-    largestPrimeFactor = 1
-    for x in range(2, number // 2 + 1):
-        if(number%x == 0):
-            if(isPrime(x)):
-                largestPrimeFactor = x
-        x += 1
-    return largestPrimeFactor
+    i = 2
+    while i ** 2< number:
+        while number % i == 0:
+            number = number / i #breaking down the initial number by prime factors, since product of prime factors is the number 
+        i = i + 1
+
+    return int(number)
+
 
 number = 600851475143 
 print(largestPrimeFactorFinder(number))

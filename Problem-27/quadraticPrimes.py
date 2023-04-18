799d05f4bf883afb80593a5f3f098075797355ca
Anton <antonilic7@gmail.com>
2023-04-17 17:27:12 -0700

    added code for solution:

--- Problem-27/quadraticPrimes.py
+++ Problem-27/quadraticPrimes.py
@@ -7,11 +7,26 @@ def isPrime(number):
             return False
     return True
 
-def findMaxPrimesForNum(a, b):
-    i = 1
-    while isPrime(i**2 + a * i + b):
+def findNumberOfPrimes(a, b):
+    i = 0
+    while isPrime(i**2 + a * i + b): #note that, by definition, since we are starting at 0, b must be prime
         i += 1
-    return i
+    return i + 1 # note that '0' is also a prime. 
 
+def solution():
+    maxPrimesUpTo = 0; alpha = 0; beta = 0
 
+    for a in range(-999, 1000):
+        print(a)
+        for b in range(0, 1000): 
+            currentPrimesUpTo = findNumberOfPrimes(a, b)
+            if currentPrimesUpTo > maxPrimesUpTo:
+                maxPrimesUpTo = currentPrimesUpTo
+                alpha = a
+                beta = b
+
+    return (alpha, beta)     
+
+if __name__ == '__main__':
+    print(solution())
 # n^2 + an + b, where |a| < 1000 and |b| ≤ 1000

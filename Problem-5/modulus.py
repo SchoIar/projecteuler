30aca9b614ece19d99ed3ddaa29266a50f0db29d
Anton <antonilic7@gmail.com>
2023-04-17 20:08:05 -0700

    used LCM approach for much faster time

--- Problem-5/modulus.py
+++ Problem-5/modulus.py
@@ -1,16 +1,21 @@
-#Anton Ilic, Feb 27, 2023
+#Anton Ilic, Feb 27, 2023, revised April 17, 2023
 #https://projecteuler.net/problem=5
 
-#Calculating primes under 20 inclusive
+## Using a new approach, considering that the LCM of 1-20 must be the selected number
 
-#Using those primes to find a number which is evenly divisible for all these numbers
-smallestEvenlyDivisible = 2 #by math: Could use 11, 13, 14, 16, 17, 18, 19, 20 for faster result, since numbers excluded are factors of listed numbers/must be true if listed are
-    
-def isEvenlyDiv(num):
-    for a in range(1,21):
-        if(num%a != 0):
-            return False
-    return True
-while(isEvenlyDiv(smallestEvenlyDivisible) is False):
-    smallestEvenlyDivisible += 2
-    print(smallestEvenlyDivisible)
+import math
+
+# Function to calculate the least common multiple (LCM) of two numbers, using GCD. 
+def findLCM(a, b):
+    lcm = (a * b) // math.gcd(a, b)
+    return lcm
+
+# Function to calculate the smallest number evenly divisible by all numbers in the range 1 to 20
+def find_smallest_evenly_divisible():
+    current_lcm = 1
+    for number in range(1, 21):
+        current_lcm = findLCM(current_lcm, number)
+    return current_lcm
+
+# Call the find_smallest_evenly_divisible function and print the result
+print(find_smallest_evenly_divisible())

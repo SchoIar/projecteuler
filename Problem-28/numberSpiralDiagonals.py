36aa735beb2141c8db1d77eb4b731ff8984e6a7c
Anton <antonilic7@gmail.com>
2023-04-20 20:13:55 -0700

    added pivots for L/R/D/U to dictate direction

--- Problem-28/numberSpiralDiagonals.py
+++ Problem-28/numberSpiralDiagonals.py
@@ -12,12 +12,18 @@ def findSumOfDiagonal(listOfNumbers):
 #Generates the 1001 x 1001 spiral
 def createSpiralOfDiagonals(spiralSize):
     the2Darray = [[0] * spiralSize for element in range(1001)] #create array to hold values
-
+    right = [1, 0]; down = [0, -1]; left = [-1, 0]; up = [0, 1]
+    pivots = [right, down, left, up]
     #set middle index (500, 500) to 1, then 501,500 to 2 then 501,501 to 3 then 500, 501 to ... etc.
     x = spiralSize // 2; y = spiralSize // 2
-    
+    currentPivot = 0
     for nextNumber in range(1, spiralSize ** 2 + 1):
-        
+
+        if currentPivot < 3:
+            currentPivot += 1 
+        else:
+            currentPivot = 0
+
         the2Darray[x][y] = nextNumber
         
         exit()

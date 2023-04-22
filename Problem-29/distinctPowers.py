4171653ee9bed5b9ea7174ce0c280f47df4ce39f
Anton <antonilic7@gmail.com>
2023-04-21 20:21:11 -0700

    final commit

--- Problem-29/distinctPowers.py
+++ Problem-29/distinctPowers.py
@@ -1,12 +1,13 @@
 # Anton Ilic, Apr 21, 2023
 # https://projecteuler.net/problem=29
 
-def solution():
+def findNumberOfDistinctPowers(aMin, bMin, aMax, bMax):
+    '''Returns the number of distinct powers, for a**b, for aMin <= a <= aMax, bMin <= b <= bMax'''
     listOfNumbers = []
-    for a in range(2, 101):
-        for b in range(2, 101):
+    for a in range(aMin, aMax + 1):
+        for b in range(bMin, bMax + 1):
             if a ** b not in listOfNumbers:
                 listOfNumbers.append(a ** b)
     return len(listOfNumbers)#len(listOfNumbers)
 
-print(solution())
+print(findNumberOfDistinctPowers(2, 2, 100, 100))

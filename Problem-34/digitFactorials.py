6a67457a7a4917db80a65574ac7d6198c21f73d3
Anton <antonilic7@gmail.com>
2023-04-24 21:21:20 -0700

    half working solution

--- Problem-34/digitFactorials.py
+++ Problem-34/digitFactorials.py
@@ -13,8 +13,18 @@ def isDigitsEqualToFactorial(number):
         factorialSum += factorial(int(element))
     if(number == factorialSum):
         return True
+    else:
+        print(f'{numberStr} is the original, {factorialSum} is sum')
   
-    
+#TODO: Figure out what maxNumber is- when does the factorial of digits become smaller then the numbe.. know that 9! is 362880
+def solution(maxNumber):
+    '''Finds all cases where the digits of the number as factorials are equal to the number'''
+    sumOfDigitsEqualToFactorial = 0
+    for currentNumber in range(3, maxNumber):
+        if isDigitsEqualToFactorial(currentNumber) == True:
+            sumOfDigitsEqualToFactorial += currentNumber
+    return sumOfDigitsEqualToFactorial
+
 # Entry point of the script
 if __name__ == '__main__':
-    pass
+    print(solution(99999))

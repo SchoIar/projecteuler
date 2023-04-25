44ec4460dfecf53451de7bbbb54cbf1d8410dea8
Anton <antonilic7@gmail.com>
2023-04-24 21:12:23 -0700

    added factorial calc function and isDigitsEqualToFactorial function:

--- Problem-34/digitFactorials.py
+++ Problem-34/digitFactorials.py
@@ -1,3 +1,20 @@
-# Anton Ilic, Apr 23, 2023
+# Anton Ilic, Apr 24, 2023
 # https://projecteuler.net/problem=34
+def factorial(element):
+    fsum = 1
+    for i in range(1, element + 1):
+        fsum = fsum * i
+    return fsum
 
+def isDigitsEqualToFactorial(number):
+    numberStr = str(number)
+    factorialSum = 0
+    for element in numberStr:
+        factorialSum += factorial(int(element))
+    if(number == factorialSum):
+        return True
+  
+    
+# Entry point of the script
+if __name__ == '__main__':
+    pass

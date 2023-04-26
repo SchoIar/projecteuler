902b9c66edd0ed75fc51aa8f6f2d2ee5255f0684
Anton <antonilic7@gmail.com>
2023-04-25 19:59:05 -0700

    formatting

--- Problem-34/digitFactorials.py
+++ Problem-34/digitFactorials.py
@@ -5,13 +5,13 @@
 def factorial(element):
     fsum = 1
     for i in range(1, element + 1):
-        fsum = fsum * i  
-    return fsum  
+        fsum = fsum * i
+    return fsum
 
 # Function to check if the sum of the factorials of the digits of a number is equal to the number itself
 def is_digits_equal_to_factorial(number, factorial_dict):
-    number_str = str(number) 
-    factorial_sum = 0 
+    number_str = str(number)
+    factorial_sum = 0
 
     for element in number_str:
         # Add the factorial of the digit to the sum of factorials
@@ -20,9 +20,9 @@ def is_digits_equal_to_factorial(number, factorial_dict):
         else:
             factorial_sum += factorial(int(element))
 
-    if(number == factorial_sum):
-        return True  
-    return False  
+    if (number == factorial_sum):
+        return True
+    return False
 
 # Function to find the sum of numbers that satisfy the is_digits_equal_to_factorial condition up to a given maximum number
 def solution(max_number):
@@ -31,13 +31,14 @@ def solution(max_number):
     for number in range(0, 10):  # Including 0 in the range to cover the factorial of 0
         factorial_dict[number] = factorial(number)
 
-    sum_of_digits_equal_to_factorial = 0 
+    sum_of_digits_equal_to_factorial = 0
     for current_number in range(3, max_number):
         if is_digits_equal_to_factorial(current_number, factorial_dict) == True:
             sum_of_digits_equal_to_factorial += current_number
-    return sum_of_digits_equal_to_factorial 
+    return sum_of_digits_equal_to_factorial
+
 
 # Entry point of the script
 if __name__ == '__main__':
     # Since 9999999 > 9! * 7, all greater numbers will be smaller than their sum of digit factorials
-    print(solution(factorial(9) * 7))  
+    print(solution(factorial(9) * 7))

878900ee9cc653ef76a2e7c6ae938afa9d4a5b28
Anton <antonilic7@gmail.com>
2023-04-25 20:21:25 -0700

    comments for logic

--- Problem-35/circularPrimes.py
+++ Problem-35/circularPrimes.py
@@ -1,3 +1,13 @@
 # Anton Ilic, Apr 25, 2023
 # https://projecteuler.net/problem=35
 
+def isPrime(number):
+    if number % 2 == 0:
+        return False
+    for i in range(3, number // 2 + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+## Safe assumption that any number with an even number or 0, 5 in it is not a circular prime, except 2 & 5...
+## The other numbers greater then 100 SHOULD be combinations of '1' '3' '7' and '9'

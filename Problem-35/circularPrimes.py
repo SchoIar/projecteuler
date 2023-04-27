89d8b2d8bd7f2d8d5cd68e8e00ebff418a813b75
Anton <antonilic7@gmail.com>
2023-04-26 21:01:11 -0700

    calculating primes up to 1 million in an efficient manner

--- Problem-35/circularPrimes.py
+++ Problem-35/circularPrimes.py
@@ -1,13 +1,41 @@
 # Anton Ilic, Apr 25, 2023
 # https://projecteuler.net/problem=35
 
-def isPrime(number):
-    if number % 2 == 0:
+def possibleCircularPrimesOver100(number):
+    #circular primes over 100 will only contain '1' '3' '7' and '9'
+    number = str(number)
+    if ('2' in number or '4' in number or '5' in number or '6' in number or '8' in number or '0' in number):
         return False
+    return True
+
+def isPrime(number):
+    if number % 2 == 0:#or possibleCircularPrimesOver100(number) == False:
+        return False
+    if number > 100:
+        if possibleCircularPrimesOver100(number) == False:
+            return False
+    
     for i in range(3, number // 2 + 1, 2):
         if number % i == 0:
             return False
     return True
+
+def solution():
+    listOfPrimes = []
+    for i in range(1, 1000001):
+        if isPrime(i):
+            print(i)
+            listOfPrimes.append(i)
+        
+    print(listOfPrimes[0:10])
+    return len(listOfPrimes)
+
+# Entry point of the script
+if __name__ == '__main__':
+    circularPrimesCount = solution()
+    print(circularPrimesCount)
+
+
 
 ## Safe assumption that any number with an even number or 0, 5 in it is not a circular prime, except 2 & 5...
 ## The other numbers greater then 100 SHOULD be combinations of '1' '3' '7' and '9'

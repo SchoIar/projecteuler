d86986fa8ec932d2372e3fad3a82593d40586f8f
Anton <antonilic7@gmail.com>
2023-04-26 21:55:12 -0700

    removed redundant comments

--- Problem-35/circularPrimes.py
+++ Problem-35/circularPrimes.py
@@ -45,44 +45,29 @@ def solution(maxNumber):
             listOfPrimes.append(i)
     return findLenOfListOfCircularPrimes((listOfPrimes))
 
-# Unit tests
-
-
+#Unit tests
 class TestCircularPrimes(unittest.TestCase):
     def test_isPrime(self):
-        # Test if isPrime returns True for prime numbers
         self.assertTrue(isPrime(2))
         self.assertTrue(isPrime(3))
         self.assertTrue(isPrime(7))
-
-        # Test if isPrime returns False for non-prime numbers
         self.assertFalse(isPrime(1))
         self.assertFalse(isPrime(4))
 
     def test_possibleCircularPrimesOver100(self):
-        # Test if possibleCircularPrimesOver100 returns True for a number with only 1, 3, 7, and 9 as digits
         self.assertTrue(possibleCircularPrimesOver100(137))
-
-        # Test if possibleCircularPrimesOver100 returns False for a number containing digits other than 1, 3, 7, and 9
         self.assertFalse(possibleCircularPrimesOver100(102))
 
     def test_findLenOfListOfCircularPrimes(self):
-        # Test findLenOfListOfCircularPrimes with a list of primes containing all circular primes below 100
         self.assertEqual(findLenOfListOfCircularPrimes(
             [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]), 13)
-
-        # Test findLenOfListOfCircularPrimes with a list of primes containing all primes below 100
         self.assertEqual(findLenOfListOfCircularPrimes(
             [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]), 13)
 
     def test_failing_tests(self):
-        # Test isPrime with an incorrect expected result
+        # Unit tests with incorrect expected results
         self.assertFalse(isPrime(3))
-
-        # Test possibleCircularPrimesOver100 with an incorrect expected result
         self.assertTrue(possibleCircularPrimesOver100(102))
-
-        # Test findLenOfListOfCircularPrimes with an incorrect expected result
         self.assertEqual(findLenOfListOfCircularPrimes(
             [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]), 14)
 

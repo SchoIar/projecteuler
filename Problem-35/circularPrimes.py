2b4e4f025f09f2cb6a9179a74d242b3aae41bc3b
Anton <antonilic7@gmail.com>
2023-04-26 21:53:23 -0700

    added comments to unit tests

--- Problem-35/circularPrimes.py
+++ Problem-35/circularPrimes.py
@@ -1,6 +1,9 @@
 # Anton Ilic, Apr 26, 2023
 # https://projecteuler.net/problem=35
 
+import unittest
+
+
 def findLenOfListOfCircularPrimes(listOfPrimes):
     circularPrimesList = []
     for prime in listOfPrimes:
@@ -26,7 +29,7 @@ def possibleCircularPrimesOver100(number):
 
 
 def isPrime(number):
-    if number % 2 == 0 or (number > 100 and possibleCircularPrimesOver100(number) == False):  
+    if number % 2 == 0 or (number > 100 and possibleCircularPrimesOver100(number) == False):
         return False
 
     for i in range(3, int(number ** 0.5) + 1, 2):
@@ -35,14 +38,58 @@ def isPrime(number):
     return True
 
 
-def solution():
+def solution(maxNumber):
     listOfPrimes = []
-    for i in range(1, 1000001):
+    for i in range(1, maxNumber + 1):
         if isPrime(i):
             listOfPrimes.append(i)
     return findLenOfListOfCircularPrimes((listOfPrimes))
 
+# Unit tests
+
+
+class TestCircularPrimes(unittest.TestCase):
+    def test_isPrime(self):
+        # Test if isPrime returns True for prime numbers
+        self.assertTrue(isPrime(2))
+        self.assertTrue(isPrime(3))
+        self.assertTrue(isPrime(7))
+
+        # Test if isPrime returns False for non-prime numbers
+        self.assertFalse(isPrime(1))
+        self.assertFalse(isPrime(4))
+
+    def test_possibleCircularPrimesOver100(self):
+        # Test if possibleCircularPrimesOver100 returns True for a number with only 1, 3, 7, and 9 as digits
+        self.assertTrue(possibleCircularPrimesOver100(137))
+
+        # Test if possibleCircularPrimesOver100 returns False for a number containing digits other than 1, 3, 7, and 9
+        self.assertFalse(possibleCircularPrimesOver100(102))
+
+    def test_findLenOfListOfCircularPrimes(self):
+        # Test findLenOfListOfCircularPrimes with a list of primes containing all circular primes below 100
+        self.assertEqual(findLenOfListOfCircularPrimes(
+            [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]), 13)
+
+        # Test findLenOfListOfCircularPrimes with a list of primes containing all primes below 100
+        self.assertEqual(findLenOfListOfCircularPrimes(
+            [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]), 13)
+
+    def test_failing_tests(self):
+        # Test isPrime with an incorrect expected result
+        self.assertFalse(isPrime(3))
+
+        # Test possibleCircularPrimesOver100 with an incorrect expected result
+        self.assertTrue(possibleCircularPrimesOver100(102))
+
+        # Test findLenOfListOfCircularPrimes with an incorrect expected result
+        self.assertEqual(findLenOfListOfCircularPrimes(
+            [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]), 14)
+
+
 # Entry point of the script
 if __name__ == '__main__':
-    circularPrimesCount = solution()
-    print(circularPrimesCount)
+    unittest.main(argv=[''], exit=False)
+    circularPrimesCount = solution(1000000)
+    print(
+        f'Number of circular primes under and up to a million: {circularPrimesCount}')

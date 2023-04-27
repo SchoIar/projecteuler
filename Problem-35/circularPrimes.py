0b8742b386ec8a9a10a13fa3d2658827f30c4e0e
Anton <antonilic7@gmail.com>
2023-04-26 21:39:19 -0700

    formatting

--- Problem-35/circularPrimes.py
+++ Problem-35/circularPrimes.py
@@ -7,7 +7,7 @@ def findLenOfListOfCircularPrimes(listOfPrimes):
         notCircularPrime = False
         primeStr = str(prime)
         for i in range(len(primeStr)):
-            shifted = primeStr[i:] + primeStr[:i] #Rotating digit by slicing
+            shifted = primeStr[i:] + primeStr[:i]  # Rotating digit by slicing
             if not isPrime(int(shifted)):
                 notCircularPrime = True
                 break
@@ -18,23 +18,25 @@ def findLenOfListOfCircularPrimes(listOfPrimes):
 
 
 def possibleCircularPrimesOver100(number):
-    #circular primes over 100 will only contain '1' '3' '7' and '9', and are rotations
+    # circular primes over 100 will only contain '1' '3' '7' and '9', and are rotations
     number = str(number)
     if ('2' in number or '4' in number or '5' in number or '6' in number or '8' in number or '0' in number):
         return False
     return True
 
+
 def isPrime(number):
-    if number % 2 == 0:#or possibleCircularPrimesOver100(number) == False:
+    if number % 2 == 0:  # or possibleCircularPrimesOver100(number) == False:
         return False
     if number > 100:
         if possibleCircularPrimesOver100(number) == False:
             return False
-    
+
     for i in range(3, int(number ** 0.5) + 1, 2):
         if number % i == 0:
             return False
     return True
+
 
 def solution():
     listOfPrimes = []
@@ -43,6 +45,7 @@ def solution():
             print(i)
             listOfPrimes.append(i)
     return findLenOfListOfCircularPrimes((listOfPrimes))
+
 
 # Entry point of the script
 if __name__ == '__main__':

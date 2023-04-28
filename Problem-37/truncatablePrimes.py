3a73874890e174dda489f2fdf96ff59937137e3e
Anton <antonilic7@gmail.com>
2023-04-27 20:50:36 -0700

    changes

--- Problem-37/truncatablePrimes.py
+++ Problem-37/truncatablePrimes.py
@@ -4,15 +4,18 @@
 import unittest
 
 def isTruncablePrime(number):
-    numStr = str(number)
+    pass
     
-    
-
 def isPrime(number):
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
     return True
+
+def solution(number):
+    listOfTruncablePrimes = []
+    
+    #loop to find truncable primes list.. Note that they must be composed without '2' '4' '5' '6' '8' '9' '0' within the digits
 
 class TestTruncatablePrimes(unittest.TestCase):
     def test_isPrime(self):

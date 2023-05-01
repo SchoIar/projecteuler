4f8d139faae410bbb132d76e2d4af7bb9a5c1a24
Anton <antonilic7@gmail.com>
2023-04-30 21:33:19 -0700

    optimized solution with itertools, permutation generation

--- Problem-41/pandigitalPrimes.py
+++ Problem-41/pandigitalPrimes.py
@@ -1,6 +1,7 @@
 # Anton Ilic, Apr 29, 2023
 # https://projecteuler.net/problem=41
 import unittest
+import itertools
 
 def isPrime(number):
     for num in range(2, int(number**0.5) + 1):
@@ -8,39 +9,29 @@ def isPrime(number):
             return False
     return True
 
-def isPandigitalToNThDigit(number):
-    lengthOfNumber = len(str(number))
-    if lengthOfNumber > 9:
-        return False
-    for digit in range(1, lengthOfNumber + 1):
-        if str(digit) in str(number):
-            pass
-        else:
-            return False
-    return True
-
-def isPandigitalPrime(number):
-    if isPandigitalToNThDigit(number) and isPrime(number):
-        return True
-    else:
-        return False
+def generatePandigitalNumber():
+    pandigitalNumbers = []
+    for n in range(1, 10):
+        for p in itertools.permutations(range(1, n + 1)):
+            num = ''
+            for element in p:
+                num = num + str(element)
+            pandigitalNumbers.append(int(num))
+    return pandigitalNumbers
 
 def solution():
-    for number in range(987654321 + 1, 1, -2):
-        if isPandigitalPrime(number):
-            return number
+    listOfPandigitalNumbers = generatePandigitalNumber()
+    for number in range(len(listOfPandigitalNumbers) - 1, 0, -1):
+        pandigital_number = listOfPandigitalNumbers[number]
+        if isPrime(pandigital_number):
+            return pandigital_number
+
 class TestPandigitalPrimes(unittest.TestCase):
-    def test_isPandigitalToNThDigit(self):
-        self.assertTrue(isPandigitalToNThDigit(123))
-    
     def test_isPrime(self):
         self.assertTrue(isPrime(13))
         self.assertFalse(isPrime(21321))
 
-    def test_isPandigitalPrime(self):
-        isPandigitalPrime(2143)
-
 if __name__ == '__main__':
-    unittest.main(argv=[''], exit=False)
+    #unittest.main(argv=[''], exit=False)
     largestPandigitalPrime = solution()
     print(largestPandigitalPrime)

6bb1acc49f9cd438d24942bb2761782f09915684
Anton <antonilic7@gmail.com>
2023-05-02 21:47:37 -0700

    more efficient numberOfDivisors() function

--- Problem-12/triangleNumber.py
+++ Problem-12/triangleNumber.py
@@ -9,14 +9,9 @@ def numberOfDivisors(number):
     #divisiorsFound = []
 
     numOfDivisors = 2 #removes redudant calculation for the number itself and 1. 
-    for i in range(2,number): #could take the sqrt to solve. 
+    for i in range(2,int(number ** 0.5) + 1): #could take the sqrt to solve. 
         if(number%i == 0):
             numOfDivisors += 1
     #return len(divisiorsFound)
     return numOfDivisors
 
-i = 0; n = 1 #i for iterated numbr, n for difference in triangle num.
-while(numberOfDivisors(i) < 500):
-    i += n; n += 1
-    
-print(i)

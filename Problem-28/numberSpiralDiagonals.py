a307c8666f74addc03b43ddd44af115c19287113
Anton <antonilic7@gmail.com>
2023-05-02 20:53:39 -0700

    logic to mathematical solution

--- Problem-28/numberSpiralDiagonals.py
+++ Problem-28/numberSpiralDiagonals.py
@@ -1,41 +1,12 @@
-# Anton Ilic, Apr 18, 2023
+# Anton Ilic, Apr 18, 2023. Revised May 2nd, 2023
 # https://projecteuler.net/problem=28
 
-def findSumOfDiagonal(listOfNumbers):
-    sumOfDiagonal = listOfNumbers[500][500] * -1
-    #Sum of indexes 00, 11, ... 1000 1000 plus 1000 0 999 1 ... 0 1000 [middle at 500, 500 is iterated thru twice]
-    for a in range(0, 1001):
-        sumOfDiagonal += listOfNumbers[a][a]
-        sumOfDiagonal += listOfNumbers[a][1000-a] 
-    return sumOfDiagonal
-
-#Generates the 1001 x 1001 spiral
-def createSpiralOfDiagonals(spiralSize):
-    the2Darray = [[0] * spiralSize for element in range(1001)] #create array to hold values
-    right = [1, 0]; down = [0, -1]; left = [-1, 0]; up = [0, 1]
-    pivots = [right, down, left, up]
-    #set middle index (500, 500) to 1, then 501,500 to 2 then 501,501 to 3 then 500, 501 to ... etc.
-    x = spiralSize // 2; y = spiralSize // 2
-    currentPivot = 0
-    for nextNumber in range(1, spiralSize ** 2 + 1):
-
-        if currentPivot < 3:
-            currentPivot += 1 
-        else:
-            currentPivot = 0
-
-        the2Darray[x][y] = nextNumber
-        
-        exit()
-    #a[x][y] = nextNum
-
-#patern relative to center
-    # +1 0
-    # +1 +1
-    # 0 +1
-    # -1 +1
-    # -1 0
-    # -1 -1
-
-
-createSpiralOfDiagonals(1001)
+#Using the mathematical patern, rather then generation of spiral 
+def calculateSum():
+    '''Calculates sum of diagonals of 1001 x 1001 matrix using the paterns as follows:
+    Sum of NE diagonal: differences of 8, 16... 
+    Sum of NW diagonal: differences of 4, 12, 20...
+    Sum of SE diagonal: differences of 6, 14, 22...
+    Sum of SW diagonal: differences of 2, 10, 18...
+    Can conclude that there are difference of 8 per 'layer', each with a different starting value
+    ''' 

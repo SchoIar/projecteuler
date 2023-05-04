e0473c8d040ebfaf7f39a466107c02be4696f047
Anton <antonilic7@gmail.com>
2023-05-04 15:45:25 -0700

    finding value for words

--- Problem-42/triangleWords.py
+++ Problem-42/triangleWords.py
@@ -1,3 +1,15 @@
-with open("data.txt", "r+") as file:
-        for line in file:
-            wordslist = line.strip().replace('"','').split(',')
+# Anton Ilic, May 4, 2023
+# https://projecteuler.net/problem=42
+def findWordValue(word):
+    wordValue = 0; word = word.lower()
+    for digit in word:
+         wordValue += ord(digit) - 96
+    return wordValue
+
+def readData():
+    with open("data.txt", "r+") as file:
+            for line in file:
+                wordslist = line.strip().replace('"','').split(',')
+    return wordslist
+
+

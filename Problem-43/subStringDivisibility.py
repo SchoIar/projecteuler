31e884425aea7b2543033d14cf30afbe5a298bad
Anton <antonilic7@gmail.com>
2023-05-05 15:59:07 -0700

    working solution for problem 43

--- Problem-43/subStringDivisibility.py
+++ Problem-43/subStringDivisibility.py
@@ -3,11 +3,34 @@
 import itertools
 
 def generatePandigitalNumber():
-    pandigitalNumbers = []
-    for n in range(1, 10):
-        for p in itertools.permutations(range(1, n + 1)):
-            num = ''
-            for element in p:
-                num = num + str(element)
-            pandigitalNumbers.append(int(num))
-    return pandigitalNumbers
+    firstPandigitalNumber = '0123456789'
+    pandigitalNumbersList = list(itertools.permutations(firstPandigitalNumber))
+    newList = []
+    for element in pandigitalNumbersList:
+        elementStr = ''
+        for thing in element:
+            elementStr = elementStr + str(thing)
+        newList.append(elementStr)
+    return newList
+
+def subStringDivisable(number):
+    numStr = str(number)
+    numList = [2, 3, 5, 7, 11, 13, 17]
+    for a in range(0, 7):
+        if not int(numStr[1 + a:4 + a]) % numList[a] == 0:
+            return False
+    return True
+
+
+def solution():
+    pandigitalNumbersList = generatePandigitalNumber()
+    sumOfDivisibleSubstrings = 0
+    for number in pandigitalNumbersList:
+        if subStringDivisable(number):
+            sumOfDivisibleSubstrings += int(number)
+    return sumOfDivisibleSubstrings
+    
+if __name__ == '__main__':
+    sum_of_divisible_substrings = solution()
+    print(sum_of_divisible_substrings)
+   

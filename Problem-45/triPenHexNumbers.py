ca77c8b0ebf1f00e8639c2fe26ae87bf3456a8cd
Anton <antonilic7@gmail.com>
2023-05-06 21:34:00 -0700

    updated solution() code, removed findNext...Number code

--- Problem-45/triPenHexNumbers.py
+++ Problem-45/triPenHexNumbers.py
@@ -3,30 +3,31 @@
 
 # Brute force method
 def findTriangleNumber(key):
-    return (key * (key + 1) / 2)
+    return int(key * (key + 1) / 2)
 
 def findPentagonalNumber(key):
-    return (key * ((3 * key) - 1) / 2)
+    return int(key * ((3 * key) - 1) / 2)
 
 def findHexogonalNumber(key):
-    return (key * (2 * key - 1))
+    return int(key * (2 * key - 1))
 
-'''def findNextTriangleNumber(memo):
-    maxIndex = len(memo) - 1
-    maxValue = memo[maxIndex]
-    key = maxValue[0]
-    memo.append((key, findNextTriangleNumber(key)))
-    return memo
-'''
-'''def solution():
-    triangleNumbersList = [(1, 1)]; pentagonalNumbersList = [(1, 1)]; hexagonalNumbersList = [(1, 1)]
-    maxNum = triangleNumbersList[len(triangleNumbersList) - 1] 
-    while(maxNum[0] < 9):
-        triangleNumbersList = findNextTriangleNumber(triangleNumbersList)
-    return triangleNumbersList'''
+def solution():
+    currentTriNumber = 286; currentPent = 166; currentHex = 144
+    triList = [findTriangleNumber(286)]; pentList = [findPentagonalNumber(currentPent)]; hexList = [findHexogonalNumber(currentHex)]
+    while ((triList[len(triList) - 1] not in pentList) and (triList[len(triList) - 1] not in hexList)):
+
+        while triList[len(triList) - 1] > pentList[len(pentList) - 1]:
+            currentPent += 1
+            pentList.append(findPentagonalNumber(currentPent))
+
+        while triList[len(triList) - 1] > hexList[len(hexList) - 1]:
+            currentHex += 1
+            hexList.append(findHexogonalNumber(currentHex))
+
+        currentTriNumber += 1
+        triList.append(findTriangleNumber(currentTriNumber))
+
+    return triList[len(triList) - 1]
 
 if __name__ == '__main__':
-    
-    '''a = solution()
-    print(a)'''
-    
+    print(solution())

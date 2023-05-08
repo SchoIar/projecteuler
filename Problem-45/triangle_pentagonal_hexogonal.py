b43a1e0d2853ec147be09600f918075d8b0bd229
Anton <antonilic7@gmail.com>
2023-05-07 19:22:41 -0700

    formatting

--- Problem-45/triangle_pentagonal_hexogonal.py
+++ Problem-45/triangle_pentagonal_hexogonal.py
@@ -2,6 +2,8 @@
 # https://projecteuler.net/problem=45
 import unittest
 # Brute force method
+
+
 def findTriangleNumber(key):
     return int(key * (key + 1) / 2)
 
@@ -16,25 +18,31 @@ def findHexogonalNumber(key):
 
 def solution(current_triangle_index, current_pentagonal_index, current_hexogonal_index):
     '''Returns a tuple of the Triangle number's 'n' value, and the triangle number, which is also a hexogonal and pentagonal number'''
-    triangleNumberList = []; pentagonalNumberList = [findPentagonalNumber(current_pentagonal_index)]; hexogonalNumberList = [findHexogonalNumber(current_hexogonal_index)]
+    triangleNumberList = []
+    pentagonalNumberList = [findPentagonalNumber(current_pentagonal_index)]
+    hexogonalNumberList = [findHexogonalNumber(current_hexogonal_index)]
     while True:
         maxTriangleNumber = findTriangleNumber(current_triangle_index)
         triangleNumberList.append(maxTriangleNumber)
 
-        #Appending pentagonal numbers up untill greater then max triangle number 
-        while(pentagonalNumberList[len(pentagonalNumberList) - 1] <= maxTriangleNumber):
+        # Appending pentagonal numbers up untill greater then max triangle number
+        while (pentagonalNumberList[len(pentagonalNumberList) - 1] <= maxTriangleNumber):
             current_pentagonal_index += 1
-            pentagonalNumberList.append(findPentagonalNumber(current_pentagonal_index))
+            pentagonalNumberList.append(
+                findPentagonalNumber(current_pentagonal_index))
 
-        #Appending hexagonal numbers up untill greater then max triangle number 
-        while(hexogonalNumberList[len(hexogonalNumberList) - 1] <= maxTriangleNumber):
+        # Appending hexagonal numbers up untill greater then max triangle number
+        while (hexogonalNumberList[len(hexogonalNumberList) - 1] <= maxTriangleNumber):
             current_hexogonal_index += 1
-            hexogonalNumberList.append(findHexogonalNumber(current_hexogonal_index))
+            hexogonalNumberList.append(
+                findHexogonalNumber(current_hexogonal_index))
 
         if maxTriangleNumber in pentagonalNumberList and maxTriangleNumber in hexogonalNumberList:
-            return maxTriangleNumber #returning the triangle number which is also pent. and hexo.
+            # returning the triangle number which is also pent. and hexo.
+            return maxTriangleNumber
 
         current_triangle_index += 1
+
 
 class TestEulerProblem(unittest.TestCase):
     def test_findTriangleNumber(self):
@@ -56,7 +64,6 @@ class TestEulerProblem(unittest.TestCase):
         self.assertEqual(findHexogonalNumber(4), 28)
 
 
-
 if __name__ == '__main__':
     unittest.main(argv=[''], exit=False)
-    print(solution(286, 165, 143)) 
+    print(solution(286, 165, 143))

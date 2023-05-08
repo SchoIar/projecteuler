310f30867824c42cb21e8dac29ee9aff358b5b68
Anton <antonilic7@gmail.com>
2023-05-07 19:22:29 -0700

    unittests

--- Problem-45/triangle_pentagonal_hexogonal.py
+++ Problem-45/triangle_pentagonal_hexogonal.py
@@ -1,6 +1,6 @@
 # Anton Ilic, May 5, 2023
 # https://projecteuler.net/problem=45
-
+import unittest
 # Brute force method
 def findTriangleNumber(key):
     return int(key * (key + 1) / 2)
@@ -36,6 +36,27 @@ def solution(current_triangle_index, current_pentagonal_index, current_hexogonal
 
         current_triangle_index += 1
 
+class TestEulerProblem(unittest.TestCase):
+    def test_findTriangleNumber(self):
+        self.assertEqual(findTriangleNumber(1), 1)
+        self.assertEqual(findTriangleNumber(2), 3)
+        self.assertEqual(findTriangleNumber(3), 6)
+        self.assertEqual(findTriangleNumber(4), 10)
+
+    def test_findPentagonalNumber(self):
+        self.assertEqual(findPentagonalNumber(1), 1)
+        self.assertEqual(findPentagonalNumber(2), 5)
+        self.assertEqual(findPentagonalNumber(3), 12)
+        self.assertEqual(findPentagonalNumber(4), 22)
+
+    def test_findHexogonalNumber(self):
+        self.assertEqual(findHexogonalNumber(1), 1)
+        self.assertEqual(findHexogonalNumber(2), 6)
+        self.assertEqual(findHexogonalNumber(3), 15)
+        self.assertEqual(findHexogonalNumber(4), 28)
+
+
 
 if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False)
     print(solution(286, 165, 143)) 

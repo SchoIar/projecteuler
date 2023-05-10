4033568c32359ddd7437a8df7004df910868ef29
Anton <antonilic7@gmail.com>
2023-05-09 18:00:21 -0700

    updated functions

--- Problem-37/truncatablePrimes.py
+++ Problem-37/truncatablePrimes.py
@@ -1,8 +1,5 @@
 # Anton Ilic, Apr 27, 2023
 # https://projecteuler.net/problem=37
-
-import unittest
-
 def isPrime(number):
     if number < 2:
         return False
@@ -12,37 +9,29 @@ def isPrime(number):
     return True
 
 def isTruncablePrime(number):
-    if number < 10 or not isPrime(number):  # single digit numbers and non-primes aren't truncatable primes
+    if not isPrime(number):  # single digit numbers and non-primes aren't truncatable primes
         return False
-
-    #Check if Truncable, and if previously found .. 
+        
+    strNumber = str(number)  
+    for i in range(1, len(strNumber)):
+        leftSlice = strNumber[i:]  # e.g., number is 378 -> 78 -> 8
+        rightSlice = strNumber[:-i]  # e.g., number is 378 -> 37 -> 3
+        if not (isPrime(int(rightSlice)) and isPrime(int(leftSlice))):
+            return False
+        
     return True
-
-
 
 def solution():
     truncatable_primes = []
-    i = 11  #single digits are not truncable
-    while len(truncatable_primes) < 11:  # 11 such truncatable primes.
+    i = 11  # single digits are not truncable
+    while len(truncatable_primes) < 11:  # there are only 11 such truncatable primes
         if isTruncablePrime(i):
             truncatable_primes.append(i)
+            
         i += 2  # only check odd numbers
     return sum(truncatable_primes)
 
-
-class TestTruncatablePrimes(unittest.TestCase):
-    def test_isPrime(self):
-        self.assertFalse(isPrime(100))
-        self.assertTrue(isPrime(13))
-
-    def test_isTruncablePrime(self):
-        self.assertFalse(isTruncablePrime(120))
-        self.assertTrue(isTruncablePrime(3797))
-        
-
-
-# Entry point of the script
 if __name__ == '__main__':
-    unittest.main(argv=[''], exit=False)
     print(solution())
+
 

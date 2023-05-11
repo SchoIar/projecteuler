74f855bf96af2b1d71676fbec1175a3e6e199485
Anton <antonilic7@gmail.com>
2023-05-10 22:13:58 -0700

    updated date'

--- Problem-44/pentagonNumbers.py
+++ Problem-44/pentagonNumbers.py
@@ -1,4 +1,4 @@
-# Anton Ilic, May 5, 2023
+# Anton Ilic, May 10, 2023
 # https://projecteuler.net/problem=44
 
 # PenNum = 3/2n^2 - n/2 -> 3/2n^2 - n/2 - PenNum = 0
@@ -7,14 +7,6 @@
 def isPentagonalNumber(number):
     if number < 1:
         return False
-    '''
-    Checks if the number is Pentagonal (see math below)
-    P = n(3n - 1)/2
-    2P = 3n^2 - n
-    0 = 3n^2 - n - 2P
-    n = [-b ± sqrt(b^2 - 4ac)] / 2a
-    n = [1 ± sqrt(1 + 24P)] / 6
-    '''
     n = (((24*number + 1)**0.5)+1)/6
     return (n == int(n))
 

657d805e474480e26a0351154df30f77bd9ac0ef
Anton <antonilic7@gmail.com>
2023-05-13 10:38:14 -0700

    format

--- Problem-47/distinctPrimeFactors.py
+++ Problem-47/distinctPrimeFactors.py
@@ -12,18 +12,19 @@ def isPrime(n):
 
 def findDistinctPrimeFactors(n, listOfFactors):
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0: #smallest number found is always the prime factor.
-            if i not in listOfFactors: #for distinct prime factors, only appending 
-                listOfFactors.append(i) 
+        if n % i == 0:  # smallest number found is always the prime factor.
+            if i not in listOfFactors:  # for distinct prime factors, only appending
+                listOfFactors.append(i)
             return findDistinctPrimeFactors(n/i, listOfFactors)
     listOfFactors.append(int(n))
     return listOfFactors
 
+
 def solution():
-    i = 4
+    i = 1
     while True:
         for num in range(0, 5):
-            arrayOfPrimeFactors = findDistinctPrimeFactors((i + num),[])
+            arrayOfPrimeFactors = findDistinctPrimeFactors((i + num), [])
             arrayLength = len(arrayOfPrimeFactors)
 
             if arrayLength == 4:
@@ -34,9 +35,10 @@ def solution():
                 print(findDistinctPrimeFactors(i + 2, []))
                 print(findDistinctPrimeFactors(i + 3, []))
                 return i
-            
+
             break
         i += 1
+
 
 if __name__ == '__main__':
     print(solution())

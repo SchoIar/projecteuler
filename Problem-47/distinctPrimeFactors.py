f311bbf986568c88f8141bf08471698f4061dd0b
Anton <antonilic7@gmail.com>
2023-05-13 10:00:52 -0700

    added __main__

--- Problem-47/distinctPrimeFactors.py
+++ Problem-47/distinctPrimeFactors.py
@@ -21,5 +21,5 @@ def findDistinctPrimeFactors(n, listOfFactors):
 def solution():
     pass
         
-
-print(findDistinctPrimeFactors(644, []))
+if __name__ == '__main__':
+    print(findDistinctPrimeFactors(644, []))

cddce38cef9e039974908b7c3a0423d067048922
Anton <antonilic7@gmail.com>
2023-05-13 14:28:39 -0700

    format

--- Problem-48/selfPowers.py
+++ Problem-48/selfPowers.py
@@ -4,11 +4,13 @@
 def findLastTenDigits(n):
     return str(n ** n)[-10:]
 
+
 def findLastTenDigitsSum(n):
     lastTenDigits = 0
     for currentNumber in range(1, n + 1):
         lastTenDigits += int(findLastTenDigits(currentNumber))
     return str(lastTenDigits)[-10:]
+
 
 if __name__ == '__main__':
     solution = findLastTenDigitsSum(1000)

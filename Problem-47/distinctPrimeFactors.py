d457576c3e7df7f84d982d75ab0129e61093a18b
Anton <antonilic7@gmail.com>
2023-05-13 10:00:04 -0700

    function to find distinct prime factors of a number

--- Problem-47/distinctPrimeFactors.py
+++ Problem-47/distinctPrimeFactors.py
@@ -1,3 +1,25 @@
 # Anton Ilic, May 12, 2023
 # https://projecteuler.net/problem=47
 
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, n ** 0.5 + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def findPrimeFactors(n, listOfFactors):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0: #smallest number found is always the prime factor.
+            if i not in listOfFactors: #for distinct prime factors, only appending 
+                listOfFactors.append(i) 
+            return findPrimeFactors(n/i, listOfFactors)
+    listOfFactors.append(int(n))
+    return listOfFactors
+
+def solution():
+    pass
+        
+
+print(findPrimeFactors(644, []))

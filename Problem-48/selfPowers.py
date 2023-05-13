578e35b7d78a1ff0fcb7f7d65a5b6574e8b1b568
Anton <antonilic7@gmail.com>
2023-05-13 14:13:50 -0700

    findLastTenDigits() implimentation

--- Problem-48/selfPowers.py
+++ Problem-48/selfPowers.py
@@ -1,2 +1,15 @@
 # Anton Ilic, May 13, 2023
 # https://projecteuler.net/problem=47
+
+def findLastTenDigits(n):
+    return str(n ** n)[-10:]
+
+def findLastTenDigitsSum(n):
+    lastTenDigits = 0
+    for currentNumber in range(1, n + 1):
+        lastTenDigits += int(findLastTenDigits(currentNumber))
+        print(lastTenDigits)
+    return str(lastTenDigits)[-10:]
+
+if __name__ == '__main__':
+    solution = findLastTenDigitsSum(1000)

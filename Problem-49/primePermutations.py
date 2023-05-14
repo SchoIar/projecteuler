2333d244a411246fdb28adecdb24596f3ee3031f
Anton <antonilic7@gmail.com>
2023-05-14 15:20:16 -0700

    checking if there are numbers with the same difference

--- Problem-49/primePermutations.py
+++ Problem-49/primePermutations.py
@@ -1,9 +1,10 @@
 # Anton Ilic, May 13, 2023
 # https://projecteuler.net/problem=49
 
-#4 digit increasing sequence...  (i) each of the three terms are prime, and, 
-#(ii) each of the 4-digit numbers are permutations of one another.
-import itertools
+# 4 digit increasing sequence...  (i) each of the three terms are prime, and,
+# (ii) each of the 4-digit numbers are permutations of one another.
+from itertools import permutations
+
 
 def isPrime(n):
     if n < 2:
@@ -13,6 +14,7 @@ def isPrime(n):
             return False
     return True
 
+
 def generatePrimes():
     '''Generates a list of all primes with 4 digits'''
     primesList = []
@@ -20,3 +22,59 @@ def generatePrimes():
         if isPrime(i):
             primesList.append(i)
     return primesList
+
+
+def findPrimePermutations():
+
+    listOfPrimes = generatePrimes()
+    listOfPrimePermutations = []
+    for number in listOfPrimes:
+        permutationsList = permutations(str(number))
+        primePermutationsList = []
+        mySet = set()
+        for element in permutationsList:
+            stringOfElement = ''
+            for thing in element:
+                stringOfElement += thing
+            intOfElement = int(stringOfElement)
+            if isPrime(intOfElement) and len(str(intOfElement)) == 4:
+                mySet.add(intOfElement)
+        for element in mySet:
+            if element in listOfPrimes:
+                if element not in primePermutationsList:
+                    primePermutationsList.append(element)
+
+        if len(primePermutationsList) >= 3:
+            if isThreeEquallySpaced(primePermutationsList) != 0:
+                listOfPrimePermutations.append(primePermutationsList)
+                print(primePermutationsList)
+    return listOfPrimePermutations
+
+
+def isThreeEquallySpaced(primesList):
+    for a in primesList:
+        #checking for the "middle" number, which has two equal differences 
+        differencesForNumber = []
+        checkedNumbers = []
+        #primesList = list(dict.fromkeys(primesList))
+        for b in primesList:
+            if a == b:
+                continue
+            
+            difference = abs(a - b) 
+            if difference in differencesForNumber and (a not in checkedNumbers and b not in checkedNumbers):
+                return difference
+            checkedNumbers.append(b)
+            differencesForNumber.append(difference)     
+    return 0
+
+
+def solution():
+
+    #findPrimePermutations()
+    primePermutationsList = findPrimePermutations()
+    print(primePermutationsList)
+
+
+if __name__ == '__main__':
+    print(solution())

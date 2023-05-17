cd760cd568bb8c72a265e5a7d7b2955c58fb35a0
Anton <antonilic7@gmail.com>
2023-05-16 21:36:20 -0700

    mathematical explaination

--- Problem-15/path.py
+++ Problem-15/path.py
@@ -10,5 +10,7 @@ def factorial(n):
 def solvebyBinomialCoefficient(n, k):
     #n represents the total number of moves (40 in this case), and k represents the number of right moves (20 in this case)
     return int(factorial(n)/(factorial(n-k)*factorial(k)))
+    #the math behind it: there are '20' possible rights, and '20' downs.. so..
+    #40 total moves / 20! * 20!
 
 print(solvebyBinomialCoefficient(40, 20))

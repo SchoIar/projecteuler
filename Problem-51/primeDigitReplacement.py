a6140d6acb6c8f3549d2e66dba6d02eb4614bb8f
Anton <antonilic7@gmail.com>
2023-05-17 23:19:22 -0700

    added isPrime()

--- Problem-51/primeDigitReplacement.py
+++ Problem-51/primeDigitReplacement.py
@@ -1,4 +1,10 @@
 # Anton Ilic, May 17, 2023
 # https://projecteuler.net/problem=51
 
-
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True

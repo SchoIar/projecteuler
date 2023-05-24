17b88deb2dd29261788a406a2a6c663224677cc4
Anton <antonilic7@gmail.com>
2023-05-23 21:41:08 -0700

    working code

--- Problem-51/primeDigitReplacement.py
+++ Problem-51/primeDigitReplacement.py
@@ -3,55 +3,45 @@
 
 from itertools import combinations
 
-
-def isPrime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-
-def replaceNumberIndex(number, index, value):
+def replace_number_indices(number, indices, replacement_digit):
     number = list(str(number))
-    number[index] = value
-    currentString = ''
-    for element in number:
-        currentString += str(element)
-    return currentString
+    for index in indices:
+        number[index] = str(replacement_digit)
+    return int(''.join(number))
 
-
-def replaceDigits(number):
-    primesSet = set()
-    numberStr = str(number)
-    # TODO: iterate through all possible replacement digits - not just the Nth digit
-    for digit in range(0, len(numberStr)):
-        primesSet = set()
-        for replacementDigit in range(0, 10):
-            # for 1 digit..
-            currentString = replaceNumberIndex(number, digit, replacementDigit)
-
-            if isPrime(int(currentString)):
-                if not currentString[0] == '0':
-                    primesSet.add(int(currentString))
-                    print(currentString)
-
-            if len(primesSet) == 8:
-                return min(primesSet)
-
+def replace_digits(number, family_size):
+    str_num = str(number)
+    len_num = len(str_num)
+    for num_indices in range(1, len_num):
+        for indices in combinations(range(len_num), num_indices):
+            prime_family = set()
+            for replacement_digit in range(10):
+                replaced_num = replace_number_indices(number, indices, replacement_digit)
+                if is_prime(replaced_num):
+                    # ignore leading zeros
+                    if len(str(replaced_num)) == len_num:
+                        prime_family.add(replaced_num)
+            if len(prime_family) == family_size:
+                return min(prime_family)
+    return None
 
 def main():
-    start = 3
-    while True:
-        start += 2
-        if isPrime(start):
-            potentialSolution = replaceDigits(start)
-            if potentialSolution != None:
-                return potentialSolution
-
+    number = 11
+    while True:  # starting from 11 and step of 2 to speed up search
+        if is_prime(number):
+            potential_solution = replace_digits(number, 8)
+            if potential_solution is not None:
+                return potential_solution
+        number += 2
+    return None
 
 if __name__ == '__main__':
-
     solution = main()
     print(solution)

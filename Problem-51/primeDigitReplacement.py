9d9a8216d25d19e452079cdc2ba0299760451b0a
Anton <antonilic7@gmail.com>
2023-05-25 21:37:22 -0700

    formatting, removing redundancies, small changes

--- Problem-51/primeDigitReplacement.py
+++ Problem-51/primeDigitReplacement.py
@@ -3,34 +3,43 @@
 
 from itertools import combinations
 
+
 def is_prime(n):
-    if n < 2: return False
+    if n < 2:
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-def replace_number_indices(number, indices, replacement_digit):
+
+def replace_number_indices(number, indices, digit):
     number = list(str(number))
     for index in indices:
-        number[index] = str(replacement_digit)
-    return int(''.join(number))
+        number[index] = str(digit)
+    replacedDigitNumber = ''
+    for element in number:
+        replacedDigitNumber = replacedDigitNumber + str(element)
+    return int(replacedDigitNumber)
+
 
 def replace_digits(number, family_size):
-    str_num = str(number)
-    len_num = len(str_num)
-    for num_indices in range(1, len_num):
-        for indices in combinations(range(len_num), num_indices):
+    numStr = str(number)
+    length_of_number = len(numStr)
+    for num_indices in range(1, length_of_number):
+        for indices in combinations(range(length_of_number), num_indices):
             prime_family = set()
             for replacement_digit in range(10):
-                replaced_num = replace_number_indices(number, indices, replacement_digit)
+                replaced_num = replace_number_indices(
+                    number, indices, replacement_digit)
                 if is_prime(replaced_num):
                     # ignore leading zeros
-                    if len(str(replaced_num)) == len_num:
+                    if len(str(replaced_num)) == length_of_number:
                         prime_family.add(replaced_num)
             if len(prime_family) == family_size:
                 return min(prime_family)
     return None
+
 
 def main():
     number = 11
@@ -40,7 +49,7 @@ def main():
             if potential_solution is not None:
                 return potential_solution
         number += 2
-    return None
+
 
 if __name__ == '__main__':
     solution = main()

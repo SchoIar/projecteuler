80b6b70ea043d94a8f50024ba2085dcd71b38fb4
Anton <antonilic7@gmail.com>
2023-05-30 20:54:26 -0700

    formatting

--- Problem-56/powerfulDigitSum.py
+++ Problem-56/powerfulDigitSum.py
@@ -9,14 +9,16 @@ def sumOfDigits(number):
         sumOfDigits += int(digit)
     return sumOfDigits
 
+
 def solution(maxNumber):
     maxSum = 0
     for a in range(1, maxNumber + 1):
         for b in range(1, maxNumber + 1):
-            currentSum = sumOfDigits(a ** b)    
+            currentSum = sumOfDigits(a ** b)
             if currentSum > maxSum:
                 maxSum = currentSum
     return maxSum
+
 
 if __name__ == '__main__':
     print(solution(100))

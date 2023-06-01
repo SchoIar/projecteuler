e7e0bd3d10014e198a14112dfe3dca06051ad20d
Anton <antonilic7@gmail.com>
2023-05-31 21:23:22 -0700

    base code, checking if 5 make a combination

---
+++ Problem-60/primePairSets.py
@@ -0,0 +1,28 @@
+# Anton Ilic, May 31, 2023
+# https://projecteuler.net/problem=60
+
+def isPrime(number):
+    if number < 2 or number % 2 == 0:
+        return False
+    for i in range(3, int(number ** 0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+def isCombinationsPrime(listOfNumbers):
+    for firstNumber in listOfNumbers:
+        for secondNumber in listOfNumbers:
+            strFirstNum = str(firstNumber)
+            strSecondNum = str(secondNumber)
+            if firstNumber == secondNumber:
+                continue
+            combinedNumber = strFirstNum + strSecondNum
+            if not isPrime(int(combinedNumber)):
+                return False
+    return True
+
+def isCombinationPrime(a, b):
+    return (isPrime(int(str(a) + str(b))) and isPrime(int(str(b) + str(a))))
+
+
+
